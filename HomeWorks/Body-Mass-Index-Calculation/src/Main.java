0460c52dcd7c220fd464bdbdb59f8a66c1cdf55f
Caner Geçgel <gecgelcaner.10@gmail.com>
2023-05-09 14:10:58 +0300

    Ödev 1

---
+++ HomeWorks/Body-Mass-Index-Calculation/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double kg ;
+        double m ;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Kilogram(kg) : ");
+        kg = input.nextDouble();
+        System.out.print("Enter Size(m) : ");
+        m = input.nextDouble();
+
+       double bodyMossIndex = kg / (m * m ) ;
+        System.out.println("Body Mass Index : " + bodyMossIndex);
+    }
+}
